c6c8649435e68d7576a74f0c5ea5d4c15dd1a9c8
Roberto Moccia <Roberto.moccia8@gmail.com>
2026-01-17 10:11:33 +0100

    Clean repository - keep only releases

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-using Foundation;
-
-namespace MoneyMindApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
